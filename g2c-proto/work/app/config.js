3a66fc002eb6b7e19acdc07ca7d778c2546026f8
Craig Oliveira <78503334+coliveir-aer@users.noreply.github.com>
2025-06-29 00:41:47 -0400

    Delete g2c-proto/work directory

--- g2c-proto/work/app/config.js
+++
@@ -1,4 +0,0 @@
-export const S3_BUCKET_NAME = 'noaa-gfs-bdp-pds';
-export const S3_BUCKET_URL_BASE = `https://${S3_BUCKET_NAME}.s3.amazonaws.com/`;
-export const S3_BUCKET_HOSTNAME = `${S3_BUCKET_NAME}.s3.us-east-1.amazonaws.com`;
-export const FETCH_TIMEOUT = 30000;
